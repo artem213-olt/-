cbfc7677a9dd939da19c8520edfa532f478afc44
artem213-olt <Andrian.artem2006@gmail.com>
2025-09-26 18:46:40 +0300

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -1,24 +1,2 @@
-#include <iostream>
-#include <langinfo.h>
-#include <string>
-#include <stack>
-int main() {
-   
-    std::vector<std::string>List = {"a","b","c"};
-
-    std::stack<int> stack;
-
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-
-    int top = stack.top();
-    stack.pop();
-
-    std::cout << top << std::endl;
-    std::cout << stack.size()<< std::endl;
-
-    return 0;
-
-
-}
+#include <unordered_map>
+std::unordered_map<std::string, int> table;
