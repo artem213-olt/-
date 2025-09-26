087c73688c87665f3e5ff88e9ff59ed9eac5590d
artem213-olt <Andrian.artem2006@gmail.com>
2025-09-26 18:46:57 +0300

    Update java.java

--- java.java
+++ java.java
@@ -1,22 +1,2 @@
-import java.util.Arrays;
-import java.util.Stack;
-
-public class CompactExample {
-    public static void main(String[] args) {
-        // Массив
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println("Массив: " + Arrays.toString(numbers));
-        
-        // Стек
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        System.out.println("Стек: " + stack);
-        
-        // Извлечение из стека
-        int top = stack.pop();
-        System.out.println("Извлечено: " + top);
-        System.out.println("Осталось: " + stack);
-    }
-}
+import java.util.HashMap;
+HashMap<String, Integer> table = new HashMap<>();
